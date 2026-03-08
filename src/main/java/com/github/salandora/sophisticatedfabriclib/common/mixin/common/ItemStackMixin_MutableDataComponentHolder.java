1a9160823b33d01dcea0d45246c54e3dd7fc9830
Salandora <salandora@gmail.com>
2026-03-08 20:20:00 +0100

    [Chore]: Fix a bug causing mining speed to persist even when throwing the tool out

--- src/main/java/com/github/salandora/sophisticatedfabriclib/common/mixin/common/ItemStackMixin_MutableDataComponentHolder.java
+++ src/main/java/com/github/salandora/sophisticatedfabriclib/common/mixin/common/ItemStackMixin_MutableDataComponentHolder.java
@@ -2,31 +2,28 @@ package com.github.salandora.sophisticatedfabriclib.common.mixin.common;
 
 import com.github.salandora.sophisticatedfabriclib.common.api.v1.extensions.component.SophisticatedMutableDataComponentHolder;
 import net.minecraft.core.component.DataComponentType;
-import net.minecraft.core.component.PatchedDataComponentMap;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin_MutableDataComponentHolder implements SophisticatedMutableDataComponentHolder {
 	@Shadow
-	@Final
-	PatchedDataComponentMap components;
+	@Nullable
+	public abstract <T> T set(DataComponentType<? super T> component, @Nullable T value);
+
+	@Shadow
+	@Nullable
+	public abstract <T> T remove(DataComponentType<? extends T> component);
 
 	@Override
 	public <T> @Nullable T sophisticatedFabricLibrary_set(DataComponentType<? super T> type, @Nullable T value) {
-		return this.components.set(type, value);
-	}
-
-	@Override
-	public <T> @Nullable T get(DataComponentType<? extends T> type) {
-		return this.components.get(type);
+		return this.set(type, value);
 	}
 
 	@Override
 	public <T> @Nullable T sophisticatedFabricLibrary_remove(DataComponentType<? extends T> type) {
-		return this.components.remove(type);
+		return this.remove(type);
 	}
 }
