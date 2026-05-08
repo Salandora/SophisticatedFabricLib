955b455587dcee43a3a679b413bb48c1462097bf
Salandora <salandora@gmail.com>
2026-05-09 00:49:59 +0200

    [Chore]: Split MutableContainerItemContext into api and impl

---
+++ src/main/java/com/github/salandora/sophisticatedfabriclib/transfer/impl/v1/MutableContainerItemContextImpl.java
@@ -0,0 +1,67 @@
+package com.github.salandora.sophisticatedfabriclib.transfer.impl.v1;
+
+import com.github.salandora.sophisticatedfabriclib.transfer.api.v1.MutableContainerItemContext;
+import net.fabricmc.fabric.api.lookup.v1.item.ItemApiLookup;
+import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.item.base.SingleStackStorage;
+import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleSlotStorage;
+import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnmodifiableView;
+
+import java.util.Collections;
+import java.util.List;
+
+public class MutableContainerItemContextImpl implements MutableContainerItemContext {
+	private final Slot slot;
+
+	public MutableContainerItemContextImpl(ItemStack stack) {
+		this.slot = new Slot(stack);
+	}
+
+	@Nullable
+	@Override
+	public <A> A find(ItemApiLookup<A, ContainerItemContext> lookup) {
+		return getItemVariant().isBlank() ? null : lookup.find(this.slot.getStack(), this);
+	}
+
+	@Override
+	public SingleSlotStorage<ItemVariant> getMainSlot() {
+		return this.slot;
+	}
+
+	@Override
+	public long insertOverflow(ItemVariant itemVariant, long maxAmount, TransactionContext transactionContext) {
+		return 0;
+	}
+
+	@Override
+	public @UnmodifiableView List<SingleSlotStorage<ItemVariant>> getAdditionalSlots() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String toString() {
+		return "MutableContainerItemContext[%d %s %s]".formatted(getMainSlot().getAmount(), getMainSlot().getResource(), getMainSlot());
+	}
+
+	private static class Slot extends SingleStackStorage {
+		private ItemStack stack;
+
+		public Slot(ItemStack stack) {
+			this.stack = stack;
+		}
+
+		@Override
+		protected ItemStack getStack() {
+			return this.stack;
+		}
+
+		@Override
+		protected void setStack(ItemStack stack) {
+			this.stack = stack;
+		}
+	}
+}
